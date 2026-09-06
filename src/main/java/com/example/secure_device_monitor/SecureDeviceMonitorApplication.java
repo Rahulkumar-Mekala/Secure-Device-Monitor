d8402a80b060e481c6a2ad1f5135de111239d77d
Rahul Kumar Mekala <raulmekala22@gmail.com>
2026-09-06 09:25:59 +0530

    first commit

---
+++ src/main/java/com/example/secure_device_monitor/SecureDeviceMonitorApplication.java
@@ -0,0 +1,14 @@
+package com.example.secure_device_monitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureDeviceMonitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecureDeviceMonitorApplication.class, args);
+		System.err.println(" Hello world");
+	}
+
+}
---
+++ src/test/java/com/example/secure_device_monitor/SecureDeviceMonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.secure_device_monitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureDeviceMonitorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
